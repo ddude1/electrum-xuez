3c7cb50b61c4f1bfd50dded82cc3451a352a9648
ddude1 <32743542+ddude1@users.noreply.github.com>
2018-05-17 09:50:04 -0400

    debug

--- lib/blockchain.py
+++ lib/blockchain.py
@@ -71,8 +71,9 @@ def serialize_header(res):
         + rev_hex(res.get('merkle_root')) \
         + int_to_hex(int(res.get('timestamp')), 4) \
         + int_to_hex(int(res.get('bits')), 4) \
-        + int_to_hex(int(res.get('nonce')), 4) \
-        + rev_hex(res.get('acc_chkpt'))
+        + int_to_hex(int(res.get('nonce')), 4) 
+     print(s)   
+       # + rev_hex(res.get('acc_chkpt'))
     return s
 
 def deserialize_header(s, height):
@@ -84,7 +85,7 @@ def deserialize_header(s, height):
     h['timestamp'] = hex_to_int(s[68:72])
     h['bits'] = hex_to_int(s[72:76])
     h['nonce'] = hex_to_int(s[76:80])
-    h['acc_chkpt'] = hash_encode(s[80:112])
+    #h['acc_chkpt'] = hash_encode(s[80:112])
     h['block_height'] = height
     return h
 
