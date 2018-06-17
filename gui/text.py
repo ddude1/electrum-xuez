ad78f863e219bc7a6468b4fbf232fd983c30f4e7
ddude <32743542+ddude1@users.noreply.github.com>
2018-06-17 00:18:43 -0400

    minor fixes

--- gui/text.py
+++ gui/text.py
@@ -37,7 +37,7 @@ class ElectrumGui:
         curses.cbreak()
         curses.start_color()
         curses.use_default_colors()
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
         curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_CYAN)
         curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
         self.stdscr.keypad(1)
